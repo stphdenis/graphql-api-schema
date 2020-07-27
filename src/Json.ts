5b503f039233997dc209885e2f5dbb6e190ab5b3
stphdenis <stph.denis@gmail.com>
2020-07-27 13:20:57 +0000

    Add regex keys

--- src/Json.ts
+++ src/Json.ts
@@ -4,58 +4,81 @@ export type Reviver = (key: string, value: any, reviver?: Reviver) => any
 
 export interface JsonOptions {
   parse: {
-    /** Interprete { "anyKey": { "$set": [...] } } as a set */
+    /**
+     * Parse `{ "anyKey": { "$set": [...] } }`
+     * 
+     * default: '$set'
+     */
     setSchema: '$set'|false
     /**
      * Keys to consider a value as a Set.
      * 
-     * Can't be '$set' if mapSchema === '$set'
+     * A key can't begin by '$'
      */
     setStringKeys: string[]|null
     /**
      * Keys to consider a value as a Set.
-     * Ex: /^\w*Set|\w*-set$/ => '*Set' et '*-set'
      * 
-     * Can't be '$set' if mapSchema === '$set'
+     * Ex: /^\w*Set|\w*-set$/ => 'xxxSet' et 'xxx-set'
+     * 
+     * A key can't begin by '$'
      */
     setRegexKeys: RegExp|null,
 
-    /** Interprete { "anyKey": { "$map": [...] } } as a map */
+    /**
+     * Parse `{ "anyKey": { "$map": [...] } }`
+     * 
+     * default: '$map'
+     */
     mapSchema: '$map'|false
     /**
      * Keys to consider a value as a Map.
      * 
-     * Can't be '$map' if mapSchema === '$map'
+     * A key can't begin by '$'
      */
     mapStringKeys: string[]|null
     /**
      * Keys to consider a value as a Map.
-     * Ex: /^\w*Map|\w*-map$/ => '*Map' et '*-map'
      * 
-     * Can't be '$map' if mapSchema === '$map'
+     * Ex: /^\w*Map|\w*-map$/ => 'xxxMap' et 'xxx-map'
+     * 
+     * A key can't begin by '$'
      */
     mapRegexKeys: RegExp|null,
 
     /**
      * '$Date' => { "anyKey": { "$date": "..." } }
+     * 
      * 'JSON&UTC' => { "anyKey": "2020-07-27T09:49:00.947Z" }
+     * 
      * 'JSON&UTC' => { "anyKey": "Mon, 27 Jul 2020 09:50:51 GMT" }
+     * 
+     * default: ['$date', 'JSON&UTC']
      */
     dateSchema: ('$date'|'JSON&UTC')[]
     /**
      * Keys to consider a value as a Date.
      * 
-     * Can't be '$date' if dateSchema === '$date'
+     * A key can't begin by '$'
      */
     dateStringKeys: string[]|null
     /**
      * Keys to consider a value as a Date.
+     * 
      * Ex: /^\w*Date|\w*-date$/ => '*Date' et '*-date'
      * 
-     * Can't be '$date' if dateSchema === '$date'
+     * A key can't begin by '$'
      */
     dateRegexKeys: RegExp|null,
-    /** Date convertion */
+    /**
+     * If dateType = "Date" then
+     * 
+     * Parse to `{ "anyKey": new Date("...") }`
+     * 
+     * If dateType = "String" then
+     * 
+     * Parse to `{ "anyKey": "..." }`
+     */
     dateType: "String"|"Date"
   }
   stringify: {
